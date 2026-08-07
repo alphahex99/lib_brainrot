a33e2b6618160a94e9581ee1588aa635cb2f57d2
alphahex99 <57233055+alphahex99@users.noreply.github.com>
2026-08-07 21:55:38 +0200

    Add wrap trickery, Remove macro trickery

---
+++ wrappers/wrap_printf.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+extern int __real_printf(const char *, ...);
+
+int __wrap_printf(const char *format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+
+    __real_printf("[!] ");
+
+    int result = vprintf(format, args);
+
+    va_end(args);
+
+    return result;
+}
---
+++ wrappers/wrap_printf.h
@@ -0,0 +1,15 @@
+#ifndef _WRAP_PRINTF_H
+#define _WRAP_PRINTF_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    int printf(const char *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WRAP_PRINTF_H */
